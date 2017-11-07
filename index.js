47e7524bc780764953983c6f86cc29192051f2ee
Robert Owen <Robert.Owen@softwire.com>
2017-11-07 16:00:49 +0000

    Renamed variables for clarity

--- index.js
+++ index.js
@@ -4,36 +4,36 @@ const mapping = {
     7 : "Bang",
 }
 
-for (let i=1; i<301; i++) {
-    let o = [];
+for (let number=1; number<301; number++) {
+    let outputBuffer = [];
 
     // build fizz/buzz/bong
     for (let v in mapping) {
-        if ((i % v) === 0) o.push(mapping[v]);
+        if ((number % v) === 0) outputBuffer.push(mapping[v]);
     }
 
     // overwrite with 'bong' if multiple of 11
-    if ((i % 11) === 0) o = ['Bong'];
+    if ((number % 11) === 0) outputBuffer = ['Bong'];
 
     // "Fezz" if multiple of 13
-    if ((i % 13) === 0) {
-        if (o.length === 0) o.push('Fezz');
+    if ((number % 13) === 0) {
+        if (outputBuffer.length === 0) outputBuffer.push('Fezz');
         else {
             // get the position where Fezz should go
             let j = 0;
-            while (j < o.length) {
-                if (o[j][0] === 'B') break;
+            while (j < outputBuffer.length) {
+                if (outputBuffer[j][0] === 'B') break;
                 j++;
             }
-            o.splice(j, 0, 'Fezz');
+            outputBuffer.splice(j, 0, 'Fezz');
         }
     }
 
-    if ((i % 17) === 0) o.reverse();
+    if ((number % 17) === 0) outputBuffer.reverse();
 
-    // if no criteria have been met, set o = i
-    if (o.length === 0) o.push(i);
+    // if no criteria have been met, set outputBuffer = number
+    if (outputBuffer.length === 0) outputBuffer.push(number);
 
     // output the result
-    console.log(i + "\t" + o.join(''));
+    console.log(number + "\t" + outputBuffer.join(''));
 }
